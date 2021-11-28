4b459bea86fa7b5273344065b793a5490319a499
Sebastian Mendoza <jhoansebastian.mendoza@alumno.iesluisvives.org>
2021-11-28 19:08:55 +0100

    Actualizando proyecto

--- src/main/java/com/example/calculadora/App.java
+++ src/main/java/com/example/calculadora/App.java
@@ -1,11 +1,8 @@
 package com.example.calculadora;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class App extends Application {
     @Override
